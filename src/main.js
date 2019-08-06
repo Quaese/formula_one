f3646d11861d5cb8942833f48407e533b16c580a
Quaese <quaese@gmx.net>
2019-08-06 18:01:10 +0200

    Add Font Awesome - #7
    * add custom/own less files

--- src/main.js
+++ src/main.js
@@ -3,45 +3,63 @@ import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/index";
 
+/* *** import own less files *** */
+import "./assets/style/index.less";
+
+/* *** load config *** */
 import { config } from "./config/config";
+
+/* *** load configure fake-backend *** */
 import { configureFakeBackend } from "./highscorelist/_tools/fake-backend.tool";
-
-// register input initializer globally
-import "./directives/init-input.directive";
-import "bootstrap/dist/css/bootstrap.css";
-
 if (config.fakeBackend) {
   console.log("fakeBackend");
   // setup fake backend
   configureFakeBackend();
 }
 
-// font-awesome 5.x
+/* *** register input initializer globally *** */
+import "./directives/init-input.directive";
+
+/* *** bootstrap *** */
+import "bootstrap/dist/css/bootstrap.css";
+
+/* *** font-awesome 5.x *** */
 // Docs see: https://www.npmjs.com/package/@fortawesome/vue-fontawesome
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
+  faCheck,
   faEdit,
+  faPencilAlt,
   faTimes,
   faTimesCircle,
   faTrashAlt
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faCheckCircle as faCheckCircleRegular,
+  faCircle as faCircleRegular,
   faTimesCircle as faTimesCircleRegular,
   faTrashAlt as faTrashAltRegular
 } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
+import {
+  FontAwesomeIcon,
+  FontAwesomeLayers
+} from "@fortawesome/vue-fontawesome";
+// add icons/fonts
 library.add(
+  faCheck,
   faEdit,
+  faPencilAlt,
   faTimes,
   faTimesCircle,
   faTrashAlt,
   faCheckCircleRegular,
+  faCircleRegular,
   faTimesCircleRegular,
   faTrashAltRegular
 );
+// register font-awesome-component globally
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component("font-awesome-layers", FontAwesomeLayers);
 
 Vue.config.productionTip = false;
 
