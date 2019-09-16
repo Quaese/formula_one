a8b9d73e8d4624bbc968b589cbede240b1cef677
Quaese <quaese@gmx.net>
2019-09-16 20:58:46 +0200

    #24 - Create Start Script
    * create env.js with ip address on server start
    * add env.js to gitignore
    * handle env.js in src/config.js

--- backend/start.js
+++ backend/start.js
@@ -1,9 +1,38 @@
 const { spawn } = require("child_process");
+const fs = require("fs");
 const helper = require("./helpers/helper");
 
+const filename = __dirname + "/" + "../src/config/" + "env.js";
 const ip = helper.getIPAddress()[0];
+
+let content;
+
+const writeEnvFile = (filename, content) => {
+  fs.writeFileSync(
+    filename,
+    content,
+    {
+      encoding: "utf8",
+      flag: "w+"
+    },
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+// create content for environment file
+content = `export const env = {"ip": "${ip}"};`;
+
+// write environment file
+writeEnvFile(filename, content);
+
+// start node server
 const nodemon = spawn("node_modules/.bin/nodemon", ["-e", "index.js"]);
 
+// event handler for spawn child processes
 nodemon.stdout.on("data", data => {
   console.log(`${data}`);
 });
--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,14 @@
+let app;
+
+try {
+  app = require("./env");
+} catch (error) {
+  app = {};
+  app.env = { ip: "localhost" };
+}
+
 const fakeBackend = false;
-const server = "localhost"; // "192.168.0.34"
+const server = app.env.ip; //"localhost"; // "192.168.0.34"
 
 export const config = {
   apiUrl: fakeBackend ? `http://${server}:4000` : `http://${server}:1337`,
