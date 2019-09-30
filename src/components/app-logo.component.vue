ec609f12d303f56edbcd5000ee0b344613e04bfd
Markus Höllein <mhoellein@epages.com>
2019-09-30 16:01:47 +0200

    * make animation working

--- src/components/app-logo.component.vue
+++ src/components/app-logo.component.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="qp-logo-wrapper">
-    <div class="qp-logo-part-spoiler"></div>
-    <div class="qp-logo-part-rearwheel"></div>
-    <div class="qp-logo-part-monocock"></div>
-    <div class="qp-logo-part-sclasse"></div>
-    <div class="qp-logo-part-front"></div>
-    <div class="qp-logo-part-bottom"></div>
-    <div class="qp-logo-part-frontwheel"></div>
-    <div class="qp-logo-part-formulaonefont"></div>
-    <div class="qp-logo-part-qp"></div>
+    <transition name="qp-logo-part-spoiler">
+      <div v-if="start" class="qp-logo-part-spoiler"></div>
+    </transition>
+    <transition name="qp-logo-part-rearwheel">
+      <div v-if="start" class="qp-logo-part-rearwheel"></div>
+    </transition>
+    <transition name="qp-logo-part-monocoque">
+      <div v-if="start" class="qp-logo-part-monocoque"></div>
+    </transition>
+    <transition name="qp-logo-part-sclasse">
+      <div v-if="start" class="qp-logo-part-sclasse"></div>
+    </transition>
+    <transition name="qp-logo-part-front">
+      <div v-if="start" class="qp-logo-part-front"></div>
+    </transition>
+    <transition name="qp-logo-part-bottom">
+      <div v-if="start" class="qp-logo-part-bottom"></div>
+    </transition>
+    <transition name="qp-logo-part-frontwheel">
+      <div v-if="start" class="qp-logo-part-frontwheel"></div>
+    </transition>
+    <transition name="qp-logo-part-formulaonefont">
+      <div v-if="start" class="qp-logo-part-formulaonefont"></div>
+    </transition>
+    <transition name="qp-logo-part-qp">
+      <div v-if="start" class="qp-logo-part-qp"></div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app-logo"
+  name: "app-logo",
+
+  data() {
+    return {
+      start: false
+    };
+  },
+
+  mounted() {
+    this.start = true;
+  }
 };
 </script>
 
