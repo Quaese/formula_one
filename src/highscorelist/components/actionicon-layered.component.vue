a632c469f3a90ab9453ece069b1e27c0d79396b7
Quaese <quaese@gmx.net>
2019-10-25 16:24:38 +0200

    Rewrite action icon component as function component

--- src/highscorelist/components/actionicon-layered.component.vue
+++ src/highscorelist/components/actionicon-layered.component.vue
@@ -1,25 +1,8 @@
-<template>
-  <font-awesome-layers
-    @click="$emit('click')"
-    :title="wrapper.title"
-    :class="
-      `fa-lg qp-action-icon qp-action-icon-layer ${
-        wrapper.class ? wrapper.class : ''
-      }`
-    "
-  >
-    <font-awesome-icon :icon="['far', 'circle']" />
-    <font-awesome-icon
-      :class="actions.class"
-      :icon="actions.icon"
-      :transform="transform"
-    />
-  </font-awesome-layers>
-</template>
-
 <script>
 export default {
   name: "action-icon",
+
+  functional: true,
 
   props: {
     wrapper: {
@@ -32,8 +15,27 @@ export default {
       type: String,
       default: "shrink-8"
     }
+  },
+
+  render: function(h, { props, listeners }) {
+    return (
+      <font-awesome-layers
+        onClick={() => {
+          listeners.click();
+        }}
+        title={props.wrapper.title}
+        class={`fa-lg qp-action-icon qp-action-icon-layer ${
+          props.wrapper.class ? props.wrapper.class : ""
+        }`}
+      >
+        <font-awesome-icon icon={["far", "circle"]} />
+        <font-awesome-icon
+          class={props.actions.class}
+          icon={props.actions.icon}
+          transform={props.transform}
+        />
+      </font-awesome-layers>
+    );
   }
 };
 </script>
-
-<style lang="less" scoped></style>
